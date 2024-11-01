6ff2258a29b2062b9947a9a04f986188f945c459
tobg <theo.beloeil.pro@gmail.com>
2024-11-01 21:03:35 +0100

    feat: add repository layer for searching public repositories

---
+++ src/models/repository.go
@@ -0,0 +1,22 @@
+package models
+
+type RepositorySearchResponse struct {
+	TotalCount        int          `json:"total_count"`
+	IncompleteResults bool         `json:"incomplete_results"`
+	Items             []Repository `json:"items"`
+}
+
+type Repository struct {
+	FullName    string `json:"full_name"`
+	Description string `json:"description"`
+	HTMLURL     string `json:"html_url"`
+	Owner       Owner  `json:"owner"`
+}
+
+type Owner struct {
+	Login     string `json:"login"`
+	ID        int    `json:"id"`
+	NodeID    string `json:"node_id"`
+	AvatarURL string `json:"avatar_url"`
+	HTMLURL   string `json:"html_url"`
+}
