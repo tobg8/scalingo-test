0e071cdd9c8eca67f8ae566b9b645589f8c4dffb
tobg <theo.beloeil.pro@gmail.com>
2024-11-01 22:04:15 +0100

    feat: add controller layer for search repositories & config entry point

--- config.go
+++ config.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"github.com/kelseyhightower/envconfig"
+	"github.com/pkg/errors"
+)
+
 type Config struct {
 	Port int `envconfig:"PORT" default:"5000"`
+}
+
+func newConfig() (*Config, error) {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		return nil, errors.Wrapf(err, "fail to build config from env")
+	}
+	return &cfg, nil
 }
